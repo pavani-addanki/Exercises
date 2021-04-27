6f1d90477d12f91edb78cf49afcf1be5a4abf6da
pavani-addanki <pavani.addanki207@gmail.com>
2021-04-28 00:21:23 +0530

    worked on exercises

--- introduction_and_environment/data_types_and_control_flow/3_grade/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_grade/Solution/solution.py
@@ -1,5 +1,19 @@
 # Code your solution here
 from provided_code import grades
 
-letter_grades = ['A','B','C','D','F']
+letter_grades = []
+
+for marks in grades:
+    if(marks >= 90 and marks <= 100):
+        letter_grades.append('A')
+    elif(marks >= 70 and marks <= 89):
+        letter_grades.append('B')
+    elif(marks >= 50 and marks <= 69):
+        letter_grades.append('C')
+    elif(marks >= 35 and marks <= 49):
+        letter_grades.append('D')
+    elif(marks < 35):
+        letter_grades.append('F')
+
+
 
--- introduction_and_environment/data_types_and_control_flow/3_set_difference/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_set_difference/Solution/solution.py
@@ -1,4 +1,5 @@
 # Code your solution here
 from provided_code import set1, set2
 
-diff = (set1 - set2) + (set2 - set1)
+diff = (set1 - set2) | (set2 - set1)
+#diff = set1.difference
